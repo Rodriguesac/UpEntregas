4c342bb65de2708fc9bd9d9149166f84a9a4a282
Rodriguesac <rodrigues.acaiecia@gmail.com>
2026-09-17 02:12:57 -0400

    fix: configure secure WebView shell for UP Gestor

--- gestor/src/main/java/com/rodriguesacai/upgestor/MainActivity.java
+++ gestor/src/main/java/com/rodriguesacai/upgestor/MainActivity.java
@@ -6,28 +6,33 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    @SuppressLint("SetJavaScriptEnabled")
+    private WebView web;
+
+    @SuppressLint({"SetJavaScriptEnabled", "AllowUniversalAccessFromFileURLs"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        WebView web = new WebView(this);
+        web = new WebView(this);
         WebSettings settings = web.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
         settings.setAllowFileAccess(true);
         settings.setAllowContentAccess(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_NEVER_ALLOW);
         web.setWebViewClient(new WebViewClient());
         web.setWebChromeClient(new WebChromeClient());
         web.loadUrl("file:///android_asset/index.html");
         setContentView(web);
-    }
 
-    @Override
-    public void onBackPressed() {
-        WebView web = findViewById(android.R.id.content).getRootView().findViewById(android.R.id.content);
-        super.onBackPressed();
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override public void handleOnBackPressed() {
+                if (web.canGoBack()) web.goBack(); else finish();
+            }
+        });
     }
 }
