26da7a5d08e9e8eac98c25c162357bf4e87543d4
Rodriguesac <rodrigues.acaiecia@gmail.com>
2026-09-17 02:11:59 -0400

    feat: add UP Gestor native shell

---
+++ gestor/src/main/java/com/rodriguesacai/upgestor/MainActivity.java
@@ -0,0 +1,33 @@
+package com.rodriguesacai.upgestor;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView web = new WebView(this);
+        WebSettings settings = web.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        web.setWebViewClient(new WebViewClient());
+        web.setWebChromeClient(new WebChromeClient());
+        web.loadUrl("file:///android_asset/index.html");
+        setContentView(web);
+    }
+
+    @Override
+    public void onBackPressed() {
+        WebView web = findViewById(android.R.id.content).getRootView().findViewById(android.R.id.content);
+        super.onBackPressed();
+    }
+}
